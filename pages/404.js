de54782dc5994cd4a243c632883c2ca22e8a096c
Abdul Moeez <abdulmoeez1225@gmail.com>
2021-11-27 22:02:28 +0500

    add the 404 page

---
+++ pages/404.js
@@ -0,0 +1,31 @@
+import Lottie from "lottie-web";
+import React, { useEffect } from "react";
+
+const PageNotFound = () => {
+  useEffect(() => {
+    Lottie.loadAnimation({
+      container: document.getElementById("animation"),
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      path: "https://assets6.lottiefiles.com/packages/lf20_afwjhfb2.json",
+    });
+  }, []);
+
+  return (
+    <div
+      style={{
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ width: "50%" }}>
+        <div id="animation">404</div>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
